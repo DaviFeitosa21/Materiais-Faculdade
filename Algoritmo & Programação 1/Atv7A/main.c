5fc0eb892197bf88a9d7deda150575720a23fda9
DaviFeitosa <90164085+DaviFeitosa21@users.noreply.github.com>
2024-06-03 08:37:22 -0300

    parte do codigo

--- Algoritmo & Programação 1/Atv7A/main.c
+++ Algoritmo & Programação 1/Atv7A/main.c
@@ -1,30 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int fibonacci(int n) {
+
+    int j = 1;
+    int i = 0;
+
+    for(int k = 1; k != n; k++) {
+
+        int t = i + j;
+        i = j;
+        j = t;
+
+        return j;
+    }
+}
+
 int main() {
 
     char opcUtiliza;
-    int idade;
+    int idade, numFibo, result_fibo;
 
     printf("Escolha uma das opcoes de utilizacao do sistema:");
-    printf("A - Calcule a idade de varias pessoas\nB - Escreva a sequencia de Fibonnaci\nC - ");
+    printf("A - Calcule a idade de varias pessoas\nB - Escreva a sequencia de Fibonnaci\nC - Leitura de numeros primos");
     scanf("%c", &opcUtiliza);
 
-    switch (opcUtiliza) {
-        case 'A' :
-            while(idade > 0){
-                printf("\nDigite uma idade: ");
-                scanf("%d", &idade);
-                printf("Idade digitada: %d\n", idade);
-            }
-            break;
-        case 'B' :
+    do {
+        switch (opcUtiliza) {
+            case 'A' :
+                while(idade > 0){
+                    printf("\nDigite uma idade: ");
+                    scanf("%d", &idade);
+                    printf("Idade digitada: %d\n", idade);
+                }
+                break;
+            case 'B' :
+                printf("Digite um numero:");
+                scanf("%d", &numFibo);
+                result_fibo = fibonacci(numFibo);
 
-
-        default:
-            break;
-    }
-    
+            default:
+                printf("tes");
+                break;
+        }
+    } while (opcUtiliza != 'A' && opcUtiliza != 'B' && opcUtiliza != 'C');
     
     return 0;
 }
